91e6a8a5670c22a39aa305f10043c239795029ae
mirandaflagg <mjf846s@missouristate.edu>
2024-02-23 23:57:16 -0600

    initial commit

---
+++ app/src/main/java/com/flagg/msu/geoquiz/QuizViewModel.kt
@@ -0,0 +1,13 @@
+package com.flagg.msu.geoquiz
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel : ViewModel() {
+    init {
+        Log.d(TAG, "ViewModel instance created")
+    }
+    override fun onCleared () {
+        super.onCleared()
+        Log.d(TAG, "ViewModel instance about to be destroyed")
+    }
+}
